df5b32cfe2458e4e1a7c5ee616ca534d12f16f25
drumevp <drumevp@coventry.ac.uk>
2021-11-24 01:34:26 +0200

    Update connection method
    The message object is now returned by the method to make it somewhat similar to websockets' on message method.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const trovobot = require('./lib/trovobot');
-const requests = require('./lib/requests');
 
 module.exports = {
-  Bot: trovobot,
-  req: requests,
+  Bot: trovobot
 }
---
+++ utils/typeConversion.js
@@ -0,0 +1,33 @@
+module.exports = types = {
+  chats: {
+    0: "chat",
+    5: "spell",
+    6: "super cap chat",
+    7: "colorful chat",
+    8: "spell chat",
+    9: "bullet screen chat",
+    5001: "sub",
+    5003: "follow",
+    5004: "join",
+    5005: "gift sub random",
+    5006: "gift sub",
+    5007: "event",
+    5008: "raid join",
+    5009: "custom spell",
+    5012: "stream status",
+    5013: "unfollow"
+  }, 
+  roles: {
+    100000: "streamer",
+    100001: "mod",
+    100002: "editor",
+    100004: "subscriber",
+    100005: "supermod",
+    100006: "follower",
+    200000: "custom role",
+    500000: "admin",
+    500001: "warden",
+    300000: "ace",
+    300001: "ace+"
+  }
+}
